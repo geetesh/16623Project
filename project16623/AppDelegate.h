8850299910934f763e055b4c867e0bf234b1449c
geeteshdubey <geeteshdubey@gmaill.com>
2016-11-10 18:33:20 -0500

    added frameworks

---
+++ project16623/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project16623
+//
+//  Created by geetesh dubey on 11/17/16.
+//  Copyright © 2016 geetesh dubey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project16623/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project16623
+//
+//  Created by geetesh dubey on 11/17/16.
+//  Copyright © 2016 geetesh dubey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
